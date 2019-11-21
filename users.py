7f2561a5cce3320d61b45ab7f9e10013ae42a212
allem40306 <allem40306@gmail.com>
2019-11-21 10:49:25 +0800

    revise user.py

--- users.py
+++ users.py
@@ -1,25 +1,35 @@
 import re
 import requests
+import json
 from bs4 import BeautifulSoup
 
-
 header={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36"}
-url="https://codeforces.com/ratings/page/1"
-print(url)
-r= requests.get(url,headers=header)
-soup = BeautifulSoup(r.text, 'html.parser')
-res = soup.select("div.ratingsDatatable table td")
 users = []
-tmp = []
-i = 0
-for r1 in res:
-    tmp2 = r1.text.replace("\r","").replace("\n","").replace(" ", "")
-    if i % 4 == 1:
-        tmp = []
-        tmp.append(tmp2)
-    elif i % 4 == 3:
-        tmp.append(tmp2)
-        users.append(tmp)
-    i += 1
-for user in users:
-    print(user)
+this_page = []
+pre_page = []
+page = 1
+while 1:
+    url="https://codeforces.com/ratings/page/" + str(page)
+    r = requests.get(url,headers=header)
+    print(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    res = soup.select("div.ratingsDatatable table td")
+    this_page = []
+    tmp = []
+    i = 0
+    for r1 in res:
+        tmp2 = r1.text.replace("\r","").replace("\n","").replace(" ", "")
+        if i % 4 == 1:
+            tmp = []
+            tmp.append(tmp2)
+        elif i % 4 == 3:
+            tmp.append(tmp2)
+            this_page.append(tmp)
+        i += 1
+    if this_page == pre_page:
+        break
+    users.append(this_page)
+    pre_page = this_page
+    page += 1
+with open('Crawler/users.json', 'w') as outfile:
+    json.dump(users, outfile)
